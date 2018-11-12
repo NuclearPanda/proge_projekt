d0e3f2b2c028619029774adf601c529f21ed7617
Johan <johanerikpukk@gmail.com>
2018-11-12 19:42:31 +0200

    Alpha 0.1 - Kuulab ja prindib tekstina mis kuulis

--- Main.py
+++ Main.py
@@ -7,19 +7,8 @@ with sr.Microphone() as source:
     audio = r.listen(source)
 print("Stopped listening.")
 
-
-credentials = r"""{
-  "type": "service_account",
-  "project_id": "***REMOVED***",
-  "private_key_id": "***REMOVED***",
-  "private_key": "-----BEGIN PRIVATE KEY-----\n***REMOVED***\n***REMOVED***\n***REMOVED***\n***REMOVED***\***REMOVED***\n***REMOVED***\***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***\n-----END PRIVATE KEY-----\n",
-  "client_email": "***REMOVED******REMOVED***.iam.gserviceaccount.com",
-  "client_id": "***REMOVED***",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/sr-user%40***REMOVED***.iam.gserviceaccount.com"
-}"""
+filename = input('Sisesta API key json faili nimi: ')
+credentials = open(filename).read()
 try:
     print("You said: " + r.recognize_google_cloud(audio, credentials_json=credentials))
 except sr.UnknownValueError:
