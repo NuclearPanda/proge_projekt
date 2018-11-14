b75f8503ded213bc30d6902a2ad85dce8498457c
Johan <johanerikpukk@gmail.com>
2018-11-15 00:59:53 +0200

    Muutsin koodi classiks ja panin tuvastatud kõne GUI-s kuvama

--- Main.py
+++ Main.py
@@ -2,50 +2,56 @@ import speech_recognition as sr
 from tkinter import *
 from tkinter import ttk
 
-
-def start_button():
-    start_btn_frame = Tk()
-    start_btn_frame.title("Action")
-    start_btn_frame.geometry("300x100")
-
-    # loome nupu
-    start_btn = ttk.Button(start_btn_frame, text="Record", command=start_btn_frame.destroy)
-    start_btn.place(x=70, y=40, width=150)
-
-    # ilmutame akna ekraanile
-    start_btn_frame.mainloop()
-
-def listening_msgbox():
-    #ToDo Aken, mis asendab printi
-
-    print("Listening! Please speak")
-
-def answer_to_GUI(you_said):
-    answer_frame = Tk()
-    answer_frame.title("To Cleverbot")
-    answer_frame.geometry("300x100")
-    answer_label = ttk.Label(answer_frame,
-                             text="You said -" + you_said)
-    answer_label.place(x=100, y=20)
-    answer_frame.mainloop()
+class speak:
+    def __init__(self,apikey):
+        self.apikey = apikey
+        self.gui = Tk()
+    def Main(self):
+        self.gui.title("Action")
+        self.gui.geometry("500x500")
+        wait = IntVar()
+        start_btn = ttk.Button(self.gui, text="Start", command=lambda: wait.set(1))
+        start_btn.place(x=100, y=100, width=150)
+        exit_label = Label(self.gui, text='Say exit to exit')
+        exit_label.place(x=275, y=100)
+        start_btn.wait_variable(wait)
+        while True:
+            speech = self.recognize()
+            if speech == 'exit ':
+                exit(0)
+            if speech is None:
+                continue
+            speech = 'You: ' + speech
+            gui_update = Label(self.gui, text=speech)
+            gui_update.pack()
+            self.gui.update_idletasks()
+            self.gui.update()
 
 
-# aktiveerib mikrofoni ja kuulab kuni on vaikus
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    start_button()
-    listening_msgbox()
-    audio = r.listen(source)
-print("Stopped listening.")
+    def recognize(self):
+        # aktiveerib mikrofoni ja kuulab kuni on vaikus
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print('Started listening.')
+            audio = r.listen(source)
+        print("Stopped listening.")
 
-filename = input('Sisesta API key json faili nimi: ')
-credentials = open(filename).read()
+        try:
+            credentials = open(self.apikey).read()
+        except FileNotFoundError:
+            print(self.apikey,'file not found')
+            exit(1)
+        try:
+            speech_to_text = r.recognize_google_cloud(audio, credentials_json=credentials)
+        except sr.UnknownValueError:
+            print("Google Cloud Speech could not understand audio")
+            return None
+        except sr.RequestError as e:
+            print("Could not request results from Google Cloud Speech service; {0}".format(e))
+            exit(1)
+        return speech_to_text
 
-try:
-    speech_to_text = r.recognize_google_cloud(audio, credentials_json=credentials)
-    answer_to_GUI(speech_to_text)
-    # print("You said: " + r.recognize_google_cloud(audio, credentials_json=credentials))
-except sr.UnknownValueError:
-    print("Google Cloud Speech could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Cloud Speech service; {0}".format(e))
+
+
+test = speak('apikey.json')
+test.Main()
--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Programmerimise aine projekt.
 
 Võtab mikrofonist heli ja muudab selle google cloud speech api-ga tekstiks ning saadab selle cleverbot apile et saada
 konversatsiooniline vastus
+
+Requirements:
+python 3.6,
+gcloud,
+google-api-python-client,
+pyaudio,
+SpeechRecognition,
