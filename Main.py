a6aa5e1534ca9f537dc66896ae75338d31e0b262
Johan <johanerikpukk@gmail.com>
2018-12-13 16:04:22 +0200

    Made listener ask for confimation before each listen and added exit button

--- Main.py
+++ Main.py
@@ -2,35 +2,50 @@ import speech_recognition as sr
 from tkinter import *
 from tkinter import ttk
 from random import randint
+import time
 
 
 class speak:
     def __init__(self,apikey):
         self.apikey = apikey # apikey failinimi
         self.gui = Tk() # tkinteri init
+        self.record_btn_text = StringVar()
+        self.wait_var = IntVar()
+        self.running = True
+
     def Main(self):
         self.gui.title("Action")
         self.gui.geometry("500x500")
-        wait = IntVar()
-        start_btn = ttk.Button(self.gui, text="Start", command=lambda: wait.set(1))
+        self.wait_var = IntVar()
+        start_btn = ttk.Button(self.gui, text="Start", command=lambda: self.wait_var.set(1))
         start_btn.grid(row=0,ipadx=210)
-        exit_label = Label(self.gui, text='Say exit to exit')
-        exit_label.grid(row=1,column=0,ipadx=210)
-        start_btn.wait_variable(wait) # ootab et start nuppu vajutataks
+        exit_button = ttk.Button(self.gui, text='Exit', command=self.shutdown)
+        exit_button.grid(row=1,column=0,ipadx=210)
+        start_btn.wait_variable(self.wait_var) # ootab et start nuppu vajutataks
         empty_line = Label(self.gui, text='') # tühi rida vahele
         empty_line.grid(row=2)
         speech_array = []
-        while True: # mainloop() asenduseks et GUI püsiks
-            speech = self.recognize()
-            #respone = self.cleverbot(speech)
-            response = 'Cleverbot vastus'
+        while self.running: # mainloop() asenduseks et GUI püsiks
+            self.record_btn_text.set('Start listening')
+            record_btn = ttk.Button(self.gui, textvariable=self.record_btn_text, command=self.wait)
+            record_btn.grid(row=0, ipadx=210)
+            record_btn.wait_variable(self.wait_var)
+            if self.running:
+                speech = self.recognize()
+            else:
+                break
+            self.record_btn_text.set('Waiting for cleverbot response...')
+            self.gui.update()
+            time.sleep(5)
+
+            response = self.cleverbot(speech)
             #speech = str(randint(0,9)) #testimiseks, kui ei saa rääkida
             if speech == 'exit ':
                 exit(0)
             if speech is None:
                 continue
             speech = 'You: ' + speech
-            #response = 'Cleverbot: ' + response
+            response = 'Cleverbot: ' + response
             speech_array.append(Label(self.gui, text=speech))
             speech_array.append(Label(self.gui, text=response))
             if len(speech_array)>20:
@@ -38,7 +53,6 @@ class speak:
                 speech_array.pop(0).destroy()
             for i in range(3,len(speech_array)+3):
                 speech_array[i-3].grid(row=i)
-                print(i)
             self.gui.update_idletasks()
             self.gui.update()
 
@@ -68,9 +82,15 @@ class speak:
         return speech_to_text
 
     def cleverbot(self,speech):
-        pass
+        return 'Cleverbot vastus läheb siia'
 
+    def wait(self):
+        self.record_btn_text.set('Listening now')
+        self.wait_var.set(1)
+        self.gui.update()
 
-
+    def shutdown(self):
+        self.running = False
+        self.wait_var.set(2)
 test = speak('apikey.json')
 test.Main()
